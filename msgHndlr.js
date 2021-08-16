58232c4b5b9e1a6da2867f300f5d4e7964d6c6ad
Jonathan Henrique <31408451+jhowbhz@users.noreply.github.com>
2021-08-16 14:10:55 -0300

    Update msgHndlr.js

--- msgHndlr.js
+++ msgHndlr.js
@@ -658,11 +658,11 @@ module.exports = msgHandler = async (client, message) => {
             if (isMedia && type === 'image') {
                 const mediaData = await decryptMedia(message, uaOverride)
                 const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`
-                await client.sendImageAsSticker(from, imageBase64, { author: "Bot do JhowJhoe", pack: "PackDoBot", keepScale: true, circle: true })
+                await client.sendImageAsSticker(from, imageBase64, { author: "Bot do JhowJhoe", pack: "PackDoBot", keepScale: true })
             } else if (quotedMsg && quotedMsg.type == 'image') {
                 const mediaData = await decryptMedia(quotedMsg, uaOverride)
                 const imageBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`
-                await client.sendImageAsSticker(from, imageBase64, { author: "Bot do JhowJhoe", pack: "PackDoBot", keepScale: true, circle: true })
+                await client.sendImageAsSticker(from, imageBase64, { author: "Bot do JhowJhoe", pack: "PackDoBot", keepScale: true })
             } else if (args.length === 2) {
                 const url = args[1]
                 if (url.match(isUrl)) {
