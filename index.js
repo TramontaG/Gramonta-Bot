e77116e95411c82b6ccb7ac3924673dad4eedd65
Jonathan Henrique <31408451+jhowbhz@users.noreply.github.com>
2021-09-05 17:54:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const start = async (client = new Client()) => {
                     break;
             
                 case 'Quem sou eu?':
-            	        await client.sendText(chat?.chatId, `Eu sou um bot, me chamou Bruce, foi desenvolvido pelo Jhon, meu codigo está disponível pra download em https://github.com/jhowbhz/bot-whatsapp`)
+            	        await client.sendText(chat?.chatId, `Eu sou um bot, me chamo Bruce, foi desenvolvido pelo Jhon, meu codigo está disponível pra download em https://github.com/jhowbhz/bot-whatsapp`)
                     break;
 
                 default:
